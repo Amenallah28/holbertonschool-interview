9416fd2aeebbc86a8a2f88f4c4a91d1bce4f4f76
Amenallah28 <amenallahmrabet9@gmail.com>
2024-10-13 13:36:21 +0100

    pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ Method to determine if all boxes are unlockable or not. """
 
+
 def canUnlockAll(boxes):
     """ Code to check if all boxes can be unlocked """
-    unlocked = [0]  
+    unlocked = [0]
     keys = boxes[0]
-    
+
     while keys:
         new_key = keys.pop()
-        
+
         if new_key not in unlocked and new_key < len(boxes):
             unlocked.append(new_key)
             keys.extend(boxes[new_key])
-    
+
     return len(unlocked) == len(boxes)
